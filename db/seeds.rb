0ffdf314dd3bb4b3b8664c8adefa4aa869a8b35c
Kirill Ilyin <cuurjol@gmail.com>
2020-03-04 11:42:50 +0300

    Create seed.rb for generation data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times { FactoryBot.create(:user) }
+10.times { FactoryBot.create(:news_item, :unpublished, user: User.all.sample) }
+20.times { FactoryBot.create(:news_item, user: User.all.sample) }
